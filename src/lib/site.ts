0d030c6bcd64e849322470f76ec19e1392102f42
ehof17 <ehof17@gmail.com>
2026-04-12 20:17:00 -0700

    update site

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Emurda Games'
 export const SITE_DESCRIPTION =
   'EmurdaGames\'s official website.'
-export const SITE_URL = 'https://emurdagames.elijahoffman.com'
+export const SITE_URL = 'emurdagames.elijah-hoffman.com'
